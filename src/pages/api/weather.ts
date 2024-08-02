fcf9a62e43e3d262ded1b842399339f9c5af489a
Alex <alexander.janke.92@gmail.com>
2024-08-02 18:44:37 +0200

    adjust weather api call params

--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -2,9 +2,8 @@ import type { APIRoute } from "astro";
 
 export const get: APIRoute = async ({ params, request }) => {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=${
-      import.meta.env.WEATHER_API
-    }&q=Ahlden&days=5&alerts=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.WEATHER_API
+    }&q=Ahlden&days=3&alerts=no`
   );
 
   return {
